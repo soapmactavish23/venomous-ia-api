9df87d2745eaf0fca967d339c3e1614d38077422
soapmactavish23 <ricknogueira1231@gmail.com>
2026-05-11 21:27:17 -0300

    Criação do endpoint ok

--- src/domain/service/identify_service.py
+++ src/domain/service/identify_service.py
@@ -9,9 +9,17 @@ class IdentifyService:
         # Aqui depois entram:
         # 1. validação do arquivo
         # 2. processamento da imagem
+        # 2.1 Checar as inferencias de todas as IAs (MobileNetV2, EfficientNetB0, RestNet50)
         # 3. processamento do áudio
-        # 4. chamada das redes neurais
-        # 5. montagem da resposta final
+        # 3.1 Transcrever o audio
+        # 5. Enviar tudo para o DeepSeek
+        # 6. montagem da resposta final
+        # 7. Apresentar 400 para caso de mensagem de Erro
+        # file_validation_service.py
+        # image_inference_service.py
+        # audio_transcription_service.py
+        # deepseek_service.py
+        # identify_service.py
 
         return {
             "scientificName": animal_name or "Animal não identificado",
